c9fa383dc6c9376869ca063f8e0adbbad0a016f4
enemrocky <craigcharltonlunga1000@gmail.com>
2024-07-31 00:27:37 -0700

    add error modal

---
+++ src/components/ErrorModal.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ErrorModal = () => {
+	return (
+		<div>
+			<header>
+				<h2>Title</h2>
+			</header>
+			<div className="content">
+				<p>Lorem ipsum dolor sit.</p>
+			</div>
+			<footer>
+				<button>close</button>
+			</footer>
+		</div>
+	);
+};
+
+export default ErrorModal;
